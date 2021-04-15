9f9e4562db6fbf0fdddf9a1a9aee9be4bd329a63
Zander Meitus <zmeitus@deloitte.com>
2021-04-14 22:21:07 -0500

    WIP persistent bugger

--- 6kyu_persistentBugger.js
+++ 6kyu_persistentBugger.js
@@ -2,12 +2,26 @@
 
 function persistence(num) {
    //code me
-
+  var z = 1;
+  var n = num;
+  var p = 1;
   // parse number into digits and multiply, length >1?
-  while(num.toString().length > 1) {
-    num.to
-  } // add to counter
+  while(n.toString().length > 1) {
+
+    var sNum = num.toString();
+    for (var i = 0; i < sNum.length; i++) {
+      p *= sNum[i];
+    }
+    
+    n = p
+    
+    // add to counter
+    if(n.toString().length > 1) {
+      z++
+    }
+    
+  }
 
   // return counter
-  return i
+  return z;
 }
